df2437aabe4916eff203b4371bdfb75630a62c49
Ian Brault <ian.brault@engineering.ucla.edu>
2019-01-12 14:26:05 -0800

    Implement scrolling for mobile (still needs van to be fixed).

--- public/js/wws.js
+++ public/js/wws.js
@@ -3,7 +3,6 @@
  */
 
 (function($) {
-    let scroll = true;
     let scrollFactor = 0.4;
     // let linkScrollSpeed = 1000;
 
@@ -13,95 +12,120 @@
     let $nav = $("#nav");
     let $van = $("#van");
 
-    let $bodyIW = $body.innerWidth();
     let $navH = $nav.innerHeight();
 
-    let vanLeftMin = -0.40 * $bodyIW;
-    let vanLeftMax = 0.44 * $bodyIW;
+    let vanLeftMin = -0.40 * $body.innerWidth();
+    let vanLeftMax = 0.44 * $body.innerWidth();
     let vanLeft = vanLeftMin;
 
-    let navBreakpoint = $bodyIW;
+    let navBreakpoint = $body.innerWidth() <= 1000 ? $body.innerHeight() : $body.innerWidth();
     let vanBreakpoint = Math.abs(vanLeftMin) + vanLeftMax;
 
-    if (browser.mobile)
-        scroll = false;
-    
-    if (scroll) {
-        (function() {
-            let normalizeWheel = function(event) {
-                let pixelStep = 10;
-                let lineHeight = 40, pageHeight = 800;
-                let sX = 0, sY = 0;
-                let pX = 0, pY = 0;
+    (function() {
+        $van.css("bottom", 40 + $navH);
 
-                if ("detail" in event)
-                    sY = event.detail;
-                else if ("wheelDelta" in event)
-                    sY = event.wheelDelta / -120;
-                else if ("wheelDeltaY" in event)
-                    sY = event.wheelDeltaY / -120;
+        let normalizeWheel = function(event) {
+            let pixelStep = 10;
+            let lineHeight = 40, pageHeight = 800;
+            let sX = 0, sY = 0;
+            let pX = 0, pY = 0;
 
-                if ("wheelDeltaX" in event)
-                    sX = event.wheelDeltaX / -120;
-                
-                if ("axis" in event && event.axis === event.HORIZONTAL_AXIS) {
-                    sX = sY;
-                    sY = 0;
+            if ("detail" in event)
+                sY = event.detail;
+            else if ("wheelDelta" in event)
+                sY = event.wheelDelta / -120;
+            else if ("wheelDeltaY" in event)
+                sY = event.wheelDeltaY / -120;
+
+            if ("wheelDeltaX" in event)
+                sX = event.wheelDeltaX / -120;
+
+            if ("axis" in event && event.axis === event.HORIZONTAL_AXIS) {
+                sX = sY;
+                sY = 0;
+            }
+
+            pX = sX * pixelStep;
+            pY = sY * pixelStep;
+            if ("deltaY" in event)
+                pY = event.deltaY;
+            if ("deltaX" in event)
+                pX = event.deltaX;
+
+            if ((pX || pY) && event.deltaMode) {
+                if (event.deltaMode === 1) {
+                    pX *= lineHeight;
+                    pY *= lineHeight;
+                } else {
+                    pX *= pageHeight;
+                    pY *= pageHeight;
                 }
+            }
 
-                pX = sX * pixelStep;
-                pY = sY * pixelStep;
-                if ("deltaY" in event)
-                    pY = event.deltaY;
-                if ("deltaX" in event)
-                    pX = event.deltaX;
+            // fallback if spin cannot be determined
+            if (pX && !sX)
+                sX = (pX < 1) ? -1 : 1;
+            if (pY && !sY)
+                sY = (pY < 1) ? -1 : 1;
 
-                if ((pX || pY) && event.deltaMode) {
-                    if (event.deltaMode === 1) {
-                        pX *= lineHeight;
-                        pY *= lineHeight;
-                    } else {
-                        pX *= pageHeight;
-                        pY *= pageHeight;
-                    }
-                }
-
-                // fallback if spin cannot be determined
-                if (pX && !sX)
-                    sX = (pX < 1) ? -1 : 1;
-                if (pY && !sY)
-                    sY = (pY < 1) ? -1 : 1;
-
-                return {
-                    spinX: sX, spinY: sY,
-                    pixelX: pX, pixelY: pY
-                };
+            return {
+                spinX: sX, spinY: sY,
+                pixelX: pX, pixelY: pY
             };
+        };
 
-            $body.on("wheel", function(event) {
-                event.preventDefault();
-                event.stopPropagation();
-                // stop link scroll
-                $bodyHtml.stop();
+        let scrollMobile = function(delta) {
+            let docTop = $document.scrollTop();
+            $document.scrollTop(docTop + delta);
+            return docTop;
+        };
 
-                let n = normalizeWheel(event.originalEvent);
-                let x = (n.pixelX !== 0 ? n.pixelX : n.pixelY);
-                let delta = Math.min(Math.abs(x), 150) * scrollFactor * (x > 0 ? 1 : -1);
+        let scrollDesktop = function(delta) {
+            let docLeft = $document.scrollLeft();
+            $document.scrollLeft(docLeft + delta);
+            return docLeft;
+        };
 
-                let docLeft = $document.scrollLeft();
-                $document.scrollLeft(docLeft + delta);
+        let scrollVan = function(delta) {
+            vanLeft += delta;
+            $van.css("left", vanLeft);
+        };
 
-                if (docLeft > navBreakpoint)
-                    $nav.css("bottom", 20);
-                else
-                    $nav.css("bottom", -20 - $navH);
+        $body.on("wheel", function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+            // stop link scroll
+            $bodyHtml.stop();
 
-                let offset = vanLeft + delta;
-                if (offset < vanLeftMax && offset > vanLeftMin && docLeft < vanBreakpoint) {
-                    vanLeft += delta;
-                    $van.css("left", vanLeft);
-                }
-            });
-        })();
-    }
+            let n = normalizeWheel(event.originalEvent);
+            let x = (n.pixelX !== 0 ? n.pixelX : n.pixelY);
+            let delta = Math.min(Math.abs(x), 150) * scrollFactor * (x > 0 ? 1 : -1);
+
+            let scroll;
+            if ($body.innerWidth() <= 1000)
+                scroll = scrollMobile(delta);
+            else
+                scroll = scrollDesktop(delta);
+
+            // show nav if past breakpoint
+            if (scroll > navBreakpoint)
+                $nav.css("bottom", 20);
+            else
+                $nav.css("bottom", -20 - $navH);
+
+            // van drive-in animation
+            let offset = vanLeft + delta;
+            if (offset < vanLeftMax && offset > vanLeftMin && scroll < vanBreakpoint)
+                scrollVan(delta);
+        });
+
+        $(window).on("resize", function() {
+            vanLeftMin = -0.40 * $("body").innerWidth();
+            vanLeftMax = 0.44 * $("body").innerWidth();
+
+            navBreakpoint = $("body").innerWidth() <= 1000 ? $("body").innerHeight() : $("body").innerWidth();
+            vanBreakpoint = Math.abs(vanLeftMin) + vanLeftMax;
+        });
+    })();
 })(jQuery);
+
