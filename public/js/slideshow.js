89ebd3a45bc9ddac55046283959451b46df94aa5
Ian Brault <ian.brault@engineering.ucla.edu>
2019-02-10 16:15:05 -0800

    Add script to async load slideshow images after page load.

--- public/js/slideshow.js
+++ public/js/slideshow.js
@@ -2,11 +2,42 @@
  * js/slideshow.js
  */
 
-$(function() {
-    $("#slideshow img:gt(0)").hide();
-    setInterval(function() {
-        $("#slideshow :first-child").fadeOut(600)
-            .next("img").fadeIn(600)
-            .end().appendTo("#slideshow");
-    }, 4000);
+const nImages = 8;
+
+const loadImage = async (n) => new Promise((resolve, reject) => {
+    let img = new Image();
+    img.onload = () => {
+        console.log("loaded image ", n);
+        resolve(img);
+    };
+    img.onerror = reject;
+    img.src = `/img/slideshow/${n}.jpg`;
+});
+
+const loadImages = async () => {
+    let promises = [];
+    for (let i = 2; i <= nImages; i++) {
+        promises.push(loadImage(i));
+    }
+
+    try {
+        let slideshow = document.getElementById("slideshow");
+        let images = await Promise.all(promises);
+        for (let i = 0; i < images.length; i++) {
+            slideshow.appendChild(images[i]);
+        }
+    } catch(err) {
+        console.error(err);
+    }
+};
+
+loadImages().then(() => {
+    $(function() {
+        $("#slideshow img:gt(0)").hide();
+        setInterval(function() {
+            $("#slideshow :first-child").fadeOut(600)
+                .next("img").fadeIn(600)
+                .end().appendTo("#slideshow");
+        }, 4000);
+    });
 });
