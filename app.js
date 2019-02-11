e154b537f99e3b4eeea74108c0fbaab7cac6d714
Ian Brault <ian.brault@engineering.ucla.edu>
2019-02-10 16:14:21 -0800

    Remove unused dependencies.

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
  * author: Ian Brault <ian.brault@engineering.ucla.edu>
  */
 
-const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
@@ -24,11 +23,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", router);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
-});
 
 // error handler
 app.use(function(err, req, res) {
