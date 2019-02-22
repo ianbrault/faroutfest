dda3a9d96b51b03a19fd4825bcdfabe406d432f0
Ian Brault <ian.brault@engineering.ucla.edu>
2019-02-22 12:48:31 -0800

    Add 2019 lineup coming soon.

--- src/js/lineup.js
+++ src/js/lineup.js
@@ -27,6 +27,10 @@ const lineup_2017_min = [
 
 const join_artist_strings = (lineup) => lineup.join(" <span class=\"dot\">•</span> ");
 
+let lineup_p_2019 = document.createElement("p");
+lineup_p_2019.id = "lineup2018";
+lineup_p_2019.innerHTML = "coming soon!";
+
 let lineup_p_2018 = document.createElement("p");
 lineup_p_2018.id = "lineup2018";
 lineup_p_2018.innerHTML = join_artist_strings(lineup_2018);
@@ -39,32 +43,50 @@ let lineup_p_2017_min = document.createElement("p");
 lineup_p_2017_min.id = "lineup2017-min";
 lineup_p_2017_min.innerHTML = join_artist_strings(lineup_2017_min);
 
+let apply_button_wrapper = document.createElement("div");
+apply_button_wrapper.className = "royce-row";
+
+let apply_button = document.createElement("div");
+apply_button.className = "button";
+apply_button.innerHTML = "apply";
+
 const unset_lineup = () => {
     while (lineup_body.firstChild)
         lineup_body.removeChild(lineup_body.firstChild);
 };
 
-let current_lineup = 2018;
-const set_lineup = (year) => {
-    lineup_title.innerHTML = `${year} Lineup`;
-    lineup_switch.innerHTML = `click to see ${year === 2018 ? "last year's" : "this year's"} lineup`;
+let lineups = [2019, 2018, 2017];
+let current_index = 0;
 
-    if (year === 2018) {
+const set_lineup = (lineup_index) => {
+    let year = lineups[lineup_index];
+    lineup_title.innerHTML = `${year} Lineup`;
+
+    if (year === 2017)
+        lineup_switch.innerHTML = "click to see this year's lineup";
+    else
+        lineup_switch.innerHTML = "click to see past lineups";
+
+    if (year === 2019) {
+        lineup_body.appendChild(lineup_p_2019);
+        apply_button_wrapper.appendChild(apply_button);
+        lineup_body.appendChild(apply_button_wrapper);
+    } else if (year === 2018) {
         lineup_body.appendChild(lineup_p_2018);
     } else {
         lineup_body.appendChild(lineup_p_2017_maj);
         lineup_body.appendChild(lineup_p_2017_min);
     }
 
-    current_lineup = year;
+    current_index = lineup_index;
 };
 
 const swap_lineups = () => {
     unset_lineup();
-    set_lineup(current_lineup === 2018 ? 2017 : 2018);
+    set_lineup((current_index + 1) % 3);
 };
 
 lineup_title.onclick = swap_lineups;
 document.getElementById("lineup-switch").onclick = swap_lineups;
 
-set_lineup(2018);
+set_lineup(0);
